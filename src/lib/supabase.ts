97a3717a17d3f28fb0b42ec8fdc2f5c94c6bfc5d
mkdevelopers2201-bit <mkdevelopers2201@gmail.com>
2026-01-26 11:39:16 +0530

    Update and rename supabase.ts to supabase-client.ts

--- src/lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://xvlnlertpfdkgvitlfsc.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh2bG5sZXJ0cGZka2d2aXRsZnNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkzMjk4NjYsImV4cCI6MjA4NDkwNTg2Nn0.y0YpOijw8yPQsuKj6zy8xaxlKHwhSwf7Y1Fm-EI-QVw';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
