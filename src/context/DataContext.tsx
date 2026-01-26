47ac11f7e5e79b9668da1a6f4703b368aee7830e
mkdevelopers2201-bit <mkdevelopers2201@gmail.com>
2026-01-26 11:14:56 +0530

    Update DataContext.tsx

--- src/context/DataContext.tsx
+++ src/context/DataContext.tsx
@@ -346,3 +346,6 @@ export const useData = () => {
   }
   return context;
 };
+
+import { createClient } from '@supabase/supabase-js';
+const supabase = createClient('https://xvlnlertpfdkgvitlfsc.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh2bG5sZXJ0cGZka2d2aXRsZnNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkzMjk4NjYsImV4cCI6MjA4NDkwNTg2Nn0.y0YpOijw8yPQsuKj6zy8xaxlKHwhSwf7Y1Fm-EI-QVw');
