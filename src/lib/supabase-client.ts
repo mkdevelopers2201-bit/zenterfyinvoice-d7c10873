a7dd7811183cb41bdc9349b22a5973e1dfac2c10
mkdevelopers2201-bit <mkdevelopers2201@gmail.com>
2026-01-26 11:52:47 +0530

    Update supabase-client.ts

--- src/lib/supabase-client.ts
+++ src/lib/supabase-client.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://xvlnlertpfdkgvitlfsc.supabase.co';
+const supabaseUrl = 'xvlnlertpfdkgvitlfsc';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh2bG5sZXJ0cGZka2d2aXRsZnNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkzMjk4NjYsImV4cCI6MjA4NDkwNTg2Nn0.y0YpOijw8yPQsuKj6zy8xaxlKHwhSwf7Y1Fm-EI-QVw';
 
 // Use 'export default' instead of just 'export const'
