87bb53c27b901308d27a4d9abdef1b71f5e1c0af
mkdevelopers2201-bit <mkdevelopers2201@gmail.com>
2026-01-26 11:00:55 +0530

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Get these from Supabase -> Settings -> API
-const supabaseUrl = 'xvlnlertpfdkgvitlfsc';
+const supabaseUrl = 'https://xvlnlertpfdkgvitlfsc.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh2bG5sZXJ0cGZka2d2aXRsZnNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkzMjk4NjYsImV4cCI6MjA4NDkwNTg2Nn0.y0YpOijw8yPQsuKj6zy8xaxlKHwhSwf7Y1Fm-EI-QVw';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
